1f5ade28b843ef8f10766ffb32fb39185cf2e12b
Enzo Martins Brito <enzo.britw@gmail.com>
2025-12-01 07:40:33 -0300

    Atualizar o App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Usando BrowserRouter
 
-// Componentes Estáticos
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-// Páginas (4 Páginas no Mínimo)
-import Home from './pages/Home'; // 1. HOME (Obrigatória)
-import Planetas from './pages/Planetas'; // 2. Planetas
+import Home from './pages/Home';
+import Planetas from './pages/Planetas'; 
 import Sobre from './pages/Sobre';
-import Contato from './pages/Contato'; // 4. Contato
+import Contato from './pages/Contato'; 
 
 function App() {
-  // Estilo para garantir que o rodapé fixo não cubra o conteúdo.
   const contentStyle = {
     paddingBottom: '80px', // Espaço suficiente para o Footer
     paddingTop: '65px',   // Espaço suficiente para o Navbar
@@ -23,11 +20,11 @@ function App() {
   
   return (
     <BrowserRouter>
-      {/* Componente fixo em todas as rotas */}
+      {}
       <Navbar /> 
 
       <main style={contentStyle}>
-        {/* Controla a exibição das páginas (SPA) */}
+        {}
         <Routes> 
           <Route path="/" element={<Home />} />
           <Route path="/planetas" element={<Planetas />} />
@@ -36,7 +33,7 @@ function App() {
         </Routes>
       </main>
 
-      {/* Componente fixo em todas as rotas */}
+      {}
       <Footer />
       
     </BrowserRouter>
